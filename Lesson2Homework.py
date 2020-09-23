a604d75dc0ddd873e7ffe50f177cadd6a8c2767f
Pierevdm <Piere@vdmerwes.com>
2020-09-23 13:35:20 +0200

    Lesson 2 and HomeWOrk

---
+++ Lesson2Homework.py
@@ -0,0 +1,21 @@
+# My Funtion Homework Assignment
+
+SongName = "Zombie"
+Artists = "Bad Wolves"
+Genre = "Hard Rock"
+
+def Songname():
+    print (SongName)
+Songname()
+
+def Artist():
+    print (Artists)
+Artist()
+
+def genre():
+    print (Genre)
+genre()
+
+
+
+    
---
+++ Lesson2Prac.py
@@ -0,0 +1,39 @@
+def addOne(Number):
+    Output = Number + 1
+    return Output
+Var = 0
+Var2 = addOne(Var)
+Var3 = addOne(Var2)
+Var4 = addOne(5)
+
+print (Var2)
+print (Var3)
+print (Var4)
+def addOneAddTwo(NumberOne,NumberTwo,Hello):
+    Output2 = NumberOne + 1
+    Output2 += NumberTwo + 5
+    Output2 = Hello
+    return Output2
+
+Sum = addOneAddTwo(1,2,"Hello")
+print (Sum)
+
+def add1(number):
+    number +=1
+    return number
+one = add1(0)
+two = add1(one)
+print (two)
+
+def Hello():
+    print("I am a Beginner")
+Hello()
+Hello()
+
+num = 1
+def func():
+    num = 4
+    print (num)
+func()
+print (num)
+
